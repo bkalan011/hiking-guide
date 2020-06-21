d42a4bff90e70423b65d67c86e99056efa472b06
Bart Kalanski <bkalanski@gmail.com>
2020-06-21 09:52:19 +0100

    Added a second api call to receive trail data

--- src/side.js
+++ src/side.js
@@ -1,4 +1,4 @@
-// fetch
+//fetch
 class Fetch {
     async getCurrent(input) {
         const myKey = "83d534996b6f4d78855bc4b7d54e6d41";
@@ -9,21 +9,14 @@ class Fetch {
             `https://api.opencagedata.com/geocode/v1/json?q=${input}&countrycode=gb&key=${myKey}`
         );
         const coordinates = await response.json();
+        console.log(coordinates);
         return coordinates;
 
     }
-    async getHikingData(coordinates) {
-        const myKey = "200804800-26d25f1775152845fd65fecad7ce8b07";
-        console.log(coordinates);
-        let lat = data.results[0].geometry.lat;
-        let long = data.results[0].geometry.lng;
-        //make request to url
 
-        const response = await fetch(
-            `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=${myKey}`
-        );
+    async getHikingData(newURL) {
+        const response = await fetch(newURL);
         const data = await response.json();
-        console.log(data);
         return data;
     }
 
@@ -36,14 +29,33 @@ class UI {
     constructor() {
         this.uiContainer = document.querySelector('.row')
     }
-
     populateUI(data) {
-        const lat = data;
-
+        const ftch = new Fetch()
+        let lat = data.results[0].geometry.lat;
+        let lng = data.results[0].geometry.lng;
+        const myKey = "200804800-26d25f1775152845fd65fecad7ce8b07";
+        let url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=60&key=${myKey}`;
+        ftch.getHikingData(url).then(newData => {
+                let hikingData = newData;
+                console.log(hikingData);
+                this.uiContainer.innerHTML = `
+                <div class="column">
+            <div class="card">
+            <img class="hiking-image" src=${hikingData.trails[0].imgSmall}></img>
+            <h3>${hikingData.trails[0].name}</h3>
+            <p>Length: ${hikingData.trails[0].length}</p>
+            <p>Summary: ${hikingData.trails[0].summary}</p>
+            <a href="${hikingData.trails[0].url}">
+            <i class="fas fa-tree"></i></a>
+        </div>
+    </div>
+                ` //hikingData.trails[0].name
+            })
+            /*
         this.uiContainer.innerHTML = ` <div class="column">
         <div class="card">
             <i class="fas fa-mountain"></i>
-            <h5>${data.results[0].components.town}</h5>
+            <h5>${data.results[0].formatted}</h5>
             <p class="title"></p>
             <p>Latitude: ${data.results[0].geometry.lat}</p>
             <p>Longitude: ${data.results[0].geometry.lng}</p>
@@ -76,7 +88,7 @@ class UI {
                     <p>Difficulty: Medium</p>
                     <i class="fas fa-tree"></i>
                 </div>
-            </div>`;
+            </div>`;*/
     }
 }
 // inst classes 
@@ -98,4 +110,4 @@ button.addEventListener("click", () => {
         //call a UI method
         ui.populateUI(data);
     });
-});
+})
