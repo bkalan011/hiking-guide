8ccb77c08dcf40d64fa4218c94dec228634e878b
Bart Kalanski <bkalanski@gmail.com>
2020-06-21 17:59:31 +0100

    First draft finished

--- src/side.js
+++ src/side.js
@@ -30,16 +30,28 @@ class UI {
         this.uiContainer = document.querySelector('.card-section')
     }
     populateUI(data) {
+
         const ftch = new Fetch()
+
         let lat = data.results[0].geometry.lat;
         let lng = data.results[0].geometry.lng;
+
         const myKey = "200804800-26d25f1775152845fd65fecad7ce8b07";
-        let url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=60&key=${myKey}`;
+
+        let url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=90&sort=distance&key=${myKey}`;
+
         ftch.getHikingData(url).then(newData => {
             let hikingData = newData;
+            let hikingPicture = hikingData.trails[0].imgSmallMed;
+            let hikingPictureTwo = hikingData.trails[1].imgSmallMed;
+            let hikingPictureThree = hikingData.trails[2].imgSmallMed;
+            hikingPicture === "" ? hikingPicture = "https://cdn2.apstatic.com/photos/hike/7016539_smallMed_1554827920.jpg" : hikingPicture;
+            hikingPictureTwo === "" ? hikingPictureTwo = "https://cdn2.apstatic.com/photos/hike/7016539_smallMed_1554827920.jpg" : hikingPictureTwo;
+            hikingPictureThree === "" ? hikingPictureThree = "https://cdn2.apstatic.com/photos/hike/7016539_smallMed_1554827920.jpg" : hikingPictureThree;
+
             console.log(hikingData);
             this.uiContainer.innerHTML = `<div class="card">
-            <div class="card-image"><img class="hiking-image"src=${hikingData.trails[0].imgSmallMed}></div>
+            <div class="card-image"><img class="hiking-image"src=${hikingPicture}></div>
             <div class="card-text">
                 <span class="date">Stars: ${hikingData.trails[0].stars}</span>
                 <h2>${hikingData.trails[0].name}</h2>
@@ -56,13 +68,13 @@ class UI {
                 </div>
                 <div class="stat">
                     <div class="value">Map</div>
-                    <div class="type"><a href="${hikingData.trails[0].url}">
-                </a>Link</div>
+                    <div class="type"><a href="${hikingData.trails[0].url}">Link
+                </a></div>
                 </div>
             </div>
         </div>
         <div class="card">
-            <div class="card-image"><img class="hiking-image"src=${hikingData.trails[1].imgSmallMed}></div>
+            <div class="card-image"><img class="hiking-image"src=${hikingPictureTwo}></div>
             <div class="card-text">
                 <span class="date">Stars: ${hikingData.trails[1].stars}</span>
                 <h2>${hikingData.trails[1].name}</h2>
@@ -79,13 +91,13 @@ class UI {
                 </div>
                 <div class="stat">
                     <div class="value">Map</div>
-                    <div class="type"><a href="${hikingData.trails[1].url}">
-                </a>Link</div>
+                    <div class="type"><a href="${hikingData.trails[1].url}">Link
+                </a></div>
                 </div>
             </div>
         </div>
         <div class="card">
-            <div class="card-image"><img class="hiking-image"src=${hikingData.trails[2].imgSmallMed}></div>
+            <div class="card-image"><img class="hiking-image"src=${hikingPicture}></div>
             <div class="card-text">
                 <span class="date">Stars: ${hikingData.trails[2].stars}</span>
                 <h2>${hikingData.trails[2].name}</h2>
@@ -102,21 +114,11 @@ class UI {
                 </div>
                 <div class="stat">
                     <div class="value">Map</div>
-                    <div class="type"><a href="${hikingData.trails[2].url}">
-                </a>Link</div>
+                    <div class="type"><a href=${hikingData.trails[2].url}>Link
+                </a></div>
                 </div>
             </div>
         </div>`
-                /* <div class="column">
-            <div class="card">
-            <img class="hiking-image" src=${hikingData.trails[0].imgSmall}></img>
-            <h3>${hikingData.trails[0].name}</h3>
-            <p>Length: ${hikingData.trails[0].length}</p>
-            <p>Summary: ${hikingData.trails[0].summary}</p>
-            <a href="${hikingData.trails[0].url}">
-            <i class="fas fa-tree"></i></a>
-        </div>
-    </div>*/
 
 
         })
